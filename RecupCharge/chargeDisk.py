34f90ee00aaeca50b01fe0038d4ec158872a9984
SuperAnimaux <RodrigoMachadoGoncalves@protonmail.com>
2025-03-22 23:52:53 +0100

    ajout fonctions utilisation disk

--- RecupCharge/chargeDisk.py
+++ RecupCharge/chargeDisk.py
@@ -1,11 +1,8 @@
 import psutil
-import time
+
 
 def recupUsageDisk():
-    disk_usage = tuple(psutil.disk_io_counters())
-    disk_usage = [(100.0 * disk_usage[i + 1]) / disk_usage[i] for i in range(0, len(disk_usage), 2)]
-    disk_usage = (disk_usage[0] + disk_usage[1] + disk_usage[2]) / 3
+    disk_usage = psutil.disk_usage('/').percent
     return disk_usage
 
-for _ in range(1000000):
-    print(recupUsageDisk())
+print(recupUsageDisk())
